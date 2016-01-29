bcd1b98b19a17e030ad23770fe9f044ea039ac81
mohamed <mohamed@Farahat>
2016-01-29 14:41:14 +0200

    new commit

---
+++ src/e002/E002.java
@@ -0,0 +1,19 @@
+
+package e002;
+
+public class E002 {
+
+    public static void main(String[] args) {
+     
+            int i,j;
+            for(i=0;i<=6;i++){
+            for(j=1;j<=7-i;j++) 
+            System.out.print("*");
+            System.out.println("");
+     }
+
+
+
+  }
+
+}
